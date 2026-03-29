e490038674afe8362a00849658e8e3b55acf3ed6
catbyte-io <catbyte.io@gmail.com>
2026-03-29 16:56:59 -0600

    Add authentication for Search SDK

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,13 @@ dependencyResolutionManagement {
         // Mapbox Maven repository
         maven {
             url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                username = "mapbox"
+                password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").get()
+            }
         }
     }
 }
